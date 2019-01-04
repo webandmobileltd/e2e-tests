ba5d707a4784b4bf799d2c43055dbf454a80fc9e
Jacek Rzeniewicz <jacek@boclips.com>
2019-01-04 08:52:14 +0000

    Add a video management spec
    [#162090870]

---
+++ cypress/integration/video_management.spec.ts
@@ -0,0 +1,20 @@
+import { VideoManagementPage } from '../page_objects/video_management/VideoManagementPage';
+
+context('Video Management', () => {
+
+  it('searching for videos journey', () => {
+    const videoManagementPage = new VideoManagementPage();
+    videoManagementPage
+      .visit()
+      .logIn()
+      .search('535')
+      .showsVideo(videos => {
+          expect(videos.length).to.equal(1);
+          const [ video ] = videos;
+          expect(video.id).to.equal('535');
+          expect(video.title).to.equal('Richard St. John: 8 secrets of success');
+          expect(video.contentPartner).to.equal('TeD');
+        }
+      )
+  });
+});
