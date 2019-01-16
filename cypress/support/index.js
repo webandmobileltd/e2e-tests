00c9df13a20075f8b59bc73b08250753c75ace91
Jacek Rzeniewicz <jacek@boclips.com>
2019-01-16 09:59:48 +0000

    Set up videos through rest, remove mysql fixtures
    [#162643123]
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,9 +15,12 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import setupE2E from './setupE2E';
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
-})
+});
+
+setupE2E();
