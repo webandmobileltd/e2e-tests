b013c850014ef965bbea03f99a82118da9b50bba
José Carlos Valero Sánchez <josecarlos@boclips.com>
2019-01-16 18:42:28 +0000

    Use working video for frontend

--- cypress/integration/frontend.spec.ts
+++ cypress/integration/frontend.spec.ts
@@ -1,23 +1,23 @@
-import {Homepage} from "../page_objects/frontend/Homepage";
+import { Homepage } from "../page_objects/frontend/Homepage";
 
-context('Frontend', () => {
-
-    it('teachers journey', () => {
-        const homepage = new Homepage();
-        const validSearchQuery = "Richard";
-        homepage
-            .visit()
-            .logIn()
-            .assertHubspotChat()
-            .assertSuccessfulLogIn()
-            .search(validSearchQuery)
-            .showsVideo(videos => {
-                    expect(videos.length).to.be.greaterThan(0,
-                        `There are no videos showing`)
-                }
-            )
-            .filterByEachSource()
-            .filterByEachDuration()
-    })
+context("Frontend", () => {
+  it("teachers journey", () => {
+    const homepage = new Homepage();
+    const validSearchQuery =
+      "Robert Neuwirth: The hidden world of shadow cities";
+    homepage
+      .visit()
+      .logIn()
+      .assertHubspotChat()
+      .assertSuccessfulLogIn()
+      .search(validSearchQuery)
+      .showsVideo(videos => {
+        expect(videos.length).to.be.greaterThan(
+          0,
+          `There are no videos showing`
+        );
+      })
+      .filterByEachSource()
+      .filterByEachDuration();
+  });
 });
-
