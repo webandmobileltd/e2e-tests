db57a395a80688db4f95fdadec846169e841d09e
Thea Kramer <thea@boclips.com>
2019-01-16 10:13:12 +0000

    Add journey for separating news content
    [#162643267]
    
    Signed-off-by: Matt Jones <matt@boclips.com>

--- cypress/integration/teachers.spec.ts
+++ cypress/integration/teachers.spec.ts
@@ -1,49 +1,80 @@
-import {TeachersHomepage} from "../page_objects/teachers/TeachersHomepage";
-import {TeachersVideoDetailsPage} from '../page_objects/teachers/TeachersVideoDetailsPage';
+import { TeachersHomepage } from "../page_objects/teachers/TeachersHomepage";
+import { TeachersVideoDetailsPage } from "../page_objects/teachers/TeachersVideoDetailsPage";
 
-context('Teachers', () => {
-    const validSearchQuery = "Ted";
-    const invalidSearchQuery = "asdfghjklkjhgf";
-    const nonEducationalSearchQuery = "Celebrities on the red carpet";
-    const email = "test@test.com";
+context("Teachers", () => {
+  const validSearchQuery = "Ted";
+  const invalidSearchQuery = "asdfghjklkjhgf";
+  const nonEducationalSearchQuery = "Celebrities on the red carpet";
+  const email = "test@test.com";
 
-    it('search journey', () => {
-        const homepage = new TeachersHomepage();
-        homepage
-            .visit()
-            .createAccount()
-            .search(invalidSearchQuery)
-            .enterEmail(email)
-            .search(validSearchQuery)
-            .showsVideo(videos => {
-                    expect(videos.length).to.be.greaterThan(0,
-                        `There are no videos showing`)
-                }
-            )
-            .isOnPage(1)
-            .goToNextPage()
-            .isOnPage(2)
-            .goToPreviousPage()
-            .isOnPage(1)
-            .goToFirstVideo()
-    });
+  it("search journey", () => {
+    const homepage = new TeachersHomepage();
+    homepage
+      .visit()
+      .createAccount()
+      .search(invalidSearchQuery)
+      .enterEmail(email)
+      .search(validSearchQuery)
+      .showsVideo(videos => {
+        expect(videos.length).to.be.greaterThan(
+          0,
+          `There are no videos showing`
+        );
+      })
+      .isOnPage(1)
+      .goToNextPage()
+      .isOnPage(2)
+      .goToPreviousPage()
+      .isOnPage(1)
+      .goToFirstVideo();
+  });
 
-    it('shows only educational videos', () => {
-        const homepage = new TeachersHomepage();
-        homepage
-            .visit()
-            .logIn()
-            .search(nonEducationalSearchQuery)
-            .noVideosShown();
-    })
+  it("shows only educational videos", () => {
+    const homepage = new TeachersHomepage();
+    homepage
+      .visit()
+      .logIn()
+      .search(nonEducationalSearchQuery)
+      .noVideosShown();
+  });
 
-    it('video details', () => {
-        const videoDetailsPage = new TeachersVideoDetailsPage('535');
-        videoDetailsPage
-            .visit()
-            .showsTitle("Richard St. John: 8 secrets of success")
-            .showsContentPartnerName("TeD")
-            .showsSubject("Maths")
-    });
+  it("video details", () => {
+    const videoDetailsPage = new TeachersVideoDetailsPage("535");
+    videoDetailsPage
+      .visit()
+      .showsTitle("Richard St. John: 8 secrets of success")
+      .showsContentPartnerName("TeD")
+      .showsSubject("Maths");
+  });
+
+  it("seperating news journey", () => {
+    const homepage = new TeachersHomepage();
+
+    const newsQuery = "news";
+
+    homepage
+      .visit()
+      .logIn()
+      .search(newsQuery)
+      .showsVideo(videos => {
+        expect(videos.length).to.be.greaterThan(
+          0,
+          `There are no videos showing`
+        );
+      })
+      .goToNewsPage()
+      .showsVideo(videos => {
+        expect(videos.length).to.be.greaterThan(
+          0,
+          `There are no videos showing`
+        );
+      })
+      .goBackToMainSearchPage()
+      .showsVideo(videos => {
+        expect(videos.length).to.be.greaterThan(
+          0,
+          `There are no videos showing`
+        );
+      });
+  });
 });
-
--- cypress/page_objects/teachers/TeachersHomepage.ts
+++ cypress/page_objects/teachers/TeachersHomepage.ts
@@ -1,11 +1,11 @@
-import { By } from '../../support/By';
-import Video from '../domain/Video';
+import { By } from "../../support/By";
+import Video from "../domain/Video";
 
 export class TeachersHomepage {
   private readonly url: string;
 
   constructor() {
-    this.url = Cypress.env('TEACHERS_BASE_URL');
+    this.url = Cypress.env("TEACHERS_BASE_URL");
   }
 
   public visit() {
@@ -14,81 +14,81 @@ export class TeachersHomepage {
   }
 
   public createAccount() {
-    cy.get(By.dataQa('create-account')).click();
+    cy.get(By.dataQa("create-account")).click();
 
-    cy.get(By.dataQa('first-name')).type('Firstname');
-    cy.get(By.dataQa('last-name')).type('Lastname');
-    cy.get(By.dataQa('subjects')).type('Subjects');
-    cy.get(By.dataQa('email')).type(Cypress.env('TEACHERS_USERNAME'));
-    cy.get(By.dataQa('password')).type(Cypress.env('TEACHERS_PASSWORD'));
-    cy.get(By.dataQa('password-confirm')).type(Cypress.env('TEACHERS_PASSWORD'));
+    cy.get(By.dataQa("first-name")).type("Firstname");
+    cy.get(By.dataQa("last-name")).type("Lastname");
+    cy.get(By.dataQa("subjects")).type("Subjects");
+    cy.get(By.dataQa("email")).type(Cypress.env("TEACHERS_USERNAME"));
+    cy.get(By.dataQa("password")).type(Cypress.env("TEACHERS_PASSWORD"));
+    cy.get(By.dataQa("password-confirm")).type(
+      Cypress.env("TEACHERS_PASSWORD")
+    );
 
-    cy.get(By.dataQa('register-button')).submit();
+    cy.get(By.dataQa("register-button")).submit();
     return this;
   }
 
   public logIn() {
-    cy.get(By.dataQa('email')).type(Cypress.env('TEACHERS_USERNAME'));
-    cy.get(By.dataQa('password')).type(Cypress.env('TEACHERS_PASSWORD'));
-    cy.get(By.dataQa('login-button')).submit();
+    cy.get(By.dataQa("email")).type(Cypress.env("TEACHERS_USERNAME"));
+    cy.get(By.dataQa("password")).type(Cypress.env("TEACHERS_PASSWORD"));
+    cy.get(By.dataQa("login-button")).submit();
     return this;
   }
 
   public search(searchQuery: string) {
-    cy.get(By.dataQa('search-input'))
+    cy.get(By.dataQa("search-input"))
       .clear()
       .type(searchQuery)
-      .type('{enter}');
+      .type("{enter}");
     return this;
   }
 
   public showsVideo(callback: (videos: Video[]) => void) {
     const videos: Video[] = [];
-    cy.get(By.dataQa('search-result'))
+    cy.get(By.dataQa("search-result"))
       .then(videoCards =>
         videoCards.each((idx, el: HTMLElement) => {
           videos.push({
-            title: el.querySelector(By.dataQa('video-title'))!.textContent!,
-            description: el.querySelector(By.dataQa('video-description'))!
-              .textContent!,
+            title: el.querySelector(By.dataQa("video-title"))!.textContent!,
+            description: el.querySelector(By.dataQa("video-description"))!
+              .textContent!
           });
-        }),
+        })
       )
       .then(() => callback(videos));
     return this;
   }
 
   enterEmail(email: string) {
-    cy.get(By.dataQa('email-address'))
+    cy.get(By.dataQa("email-address"))
       .clear()
       .type(email);
     return this;
   }
 
   copyFirstLink() {
-    cy.get(By.dataQa('copy-link'))
+    cy.get(By.dataQa("copy-link"))
       .first()
       .click();
     return this;
   }
 
   visitCopiedLink() {
-    cy.get('.ant-notification-notice-message')
-      .then((message) => {
-        cy.visit(message.text());
-      });
+    cy.get(".ant-notification-notice-message").then(message => {
+      cy.visit(message.text());
+    });
     return this;
   }
 
   playVideo() {
-    cy.get('.boclips-player')
-      .click();
+    cy.get(".boclips-player").click();
     return this;
-
   }
 
   isOnPage(pageNumber: number) {
-    let activeElementInPagination = '[data-qa=\'pagination\'] .ant-pagination-item-active a';
+    let activeElementInPagination =
+      "[data-qa='pagination'] .ant-pagination-item-active a";
     cy.get(activeElementInPagination).then(elements => {
       elements.each((idx, element: HTMLElement) => {
         let textContent = element!.textContent;
@@ -99,23 +99,41 @@ export class TeachersHomepage {
   }
 
   goToNextPage() {
-    cy.get('.ant-pagination-next').click();
+    cy.get(".ant-pagination-next").click();
     return this;
   }
 
   goToPreviousPage() {
-    cy.get('.ant-pagination-prev').click();
+    cy.get(".ant-pagination-prev").click();
     return this;
   }
 
   goToFirstVideo() {
-    cy.get(By.dataQa('link-to-details')).first().click();
-    expect(cy.get(By.dataQa('video-details-title')));
+    cy.get(By.dataQa("link-to-details"))
+      .first()
+      .click();
+    expect(cy.get(By.dataQa("video-details-title")));
     return this;
   }
 
   noVideosShown() {
-    cy.get(By.dataQa('search-zero-results'));
+    cy.get(By.dataQa("search-zero-results"));
+    return this;
+  }
+
+  goToNewsPage() {
+    cy.get(By.dataQa("news-side-panel"))
+      .find("button")
+      .click();
+
+    return this;
+  }
+
+  goBackToMainSearchPage() {
+    cy.get(By.dataQa("news-header"))
+      .find("button")
+      .click();
+
     return this;
   }
 }
