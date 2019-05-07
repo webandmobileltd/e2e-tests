9edd26b3a9310b8c427f850c3a3b487f2efccd3d
Matt Jones <matt@boclips.com>
2019-05-07 16:39:30 +0200

    Increase default width to be truly desktop

--- cypress/integration/viewports.ts
+++ cypress/integration/viewports.ts
@@ -1,6 +1,6 @@
 import ViewPort from '../page_objects/types/ViewPort';
 
 export const sizes: ViewPort[] = [
-  { height: 660, width: 1000, isMobile: false },
+  { height: 660, width: 1280, isMobile: false },
   { height: 667, width: 375, isMobile: true },
 ];
