6aaa04760c21d0ba15c19c1f8dada8dfc3ad9e35
Rafal Lewandowski <rafal@boclips.com>
2019-07-17 16:58:27 +0200

    Implement logic to avoid indexing race conditions
    E2E setup scripts query for videos straight after inserting them, so
    it's possible video service did not finish indexing in time and query
    will return 0 results in that case.
    
    [#166633060]

--- setup/videoApi.js
+++ setup/videoApi.js
@@ -15,8 +15,6 @@ async function insertVideo(video, token) {
 }
 
 async function findOneVideo(query, token) {
-  console.log(Constants.API_URL);
-
   const response = await fetch(Constants.API_URL + `/v1/videos?query=${query}&page=0&size=1`, {
     method: "GET",
     headers: {
@@ -27,13 +25,20 @@ async function findOneVideo(query, token) {
 
   const payload = await response.json();
 
-  console.log('JSON', payload);
+  console.log('Single video lookup result', payload);
   const video = payload._embedded.videos[0];
   return video.id;
 }
 
-async function findVideos(query, token) {
-  console.log(Constants.API_URL);
+async function findVideos(query, token, attempts) {
+  // Attempts logic implemented due to a race condition where videos are queried
+  // straight after inserting and indexing did not finish in time.
+  if (attempts === undefined) {
+    attempts = 1
+  }
+  if (attempts > 10) {
+    throw new Error('Max query attempts reached')
+  }
 
   const response = await fetch(Constants.API_URL + `/v1/videos?query=${query}&page=0&size=1`, {
     method: "GET",
@@ -45,9 +50,18 @@ async function findVideos(query, token) {
 
   const payload = await response.json();
 
-  console.log('JSON', payload);
+  console.log('Videos query result:', payload);
+
+  if (payload._embedded.videos.length === 0) {
+    await sleepForMillis(1000);
+    return findVideos(query, token, attempts + 1);
+  }
 
   return payload._embedded.videos;
+}
+
+async function sleepForMillis(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
 }
 
 module.exports = {
