eebccbcd7023f33c7844ba3e3dff39a6460752aa
Boisababy Clips <bob@fleming.nowhere>
2019-10-03 17:57:45 +0100

    Revert to the last passing commit

--- cypress/page_objects/backoffice/BackofficePage.ts
+++ cypress/page_objects/backoffice/BackofficePage.ts
@@ -81,12 +81,6 @@ export class BackofficePage {
 
   public updateOrderCurrency() {
     cy.get(By.dataQa('edit-currency')).click();
-    cy.get(By.dataQa('currency-select')).click();
-    cy.get(`.ant-select-dropdown-menu-item`)
-      .contains('USD')
-      .scrollIntoView()
-      .click();
-    cy.get(By.dataQa('inline-save')).click();
     return this;
   }
 
