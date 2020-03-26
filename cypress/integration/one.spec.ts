e4b95019f40585ccdbe712e5430e0606795ad5ff
Remigiusz Oleszczuk <remy@boclips.com>
2020-03-26 14:37:05 +0100

    Add creating collections e2e test
    [#171568562]

---
+++ cypress/integration/one.spec.ts
@@ -0,0 +1,16 @@
+import { BackofficePage } from '../page_objects/backoffice/BackofficePage';
+
+context('Backoffice test', () => {
+  const backoffice = new BackofficePage();
+
+  it('should go to collections page and create a collection', () => {
+    backoffice
+      .visit()
+      .logIn()
+      .goToCollectionsPage()
+      .openCollectionsModal()
+      .fillCollectionDetails()
+      .saveCollection()
+      .findCreatedCollection();
+  });
+});
